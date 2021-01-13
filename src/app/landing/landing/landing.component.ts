c1bbf384a08cefa7576e7e35d27ff788bdd7e865
Sunit <sunit.sinha@dell.com>
2021-01-13 19:40:15 +0530

    updates

--- src/app/landing/landing/landing.component.ts
+++ src/app/landing/landing/landing.component.ts
@@ -11,8 +11,10 @@ import { LandingService } from '../landing.service';
 })
 export class LandingComponent implements OnInit {
 
-  @ViewChild('launch') launch: any;
-  @ViewChild('land') land: any;
+  @ViewChild('launchTrue') launchTrue: any;
+  @ViewChild('launchFail') launchFail: any;
+  @ViewChild('landTrue') landTrue: any;
+  @ViewChild('landFail') landFail: any;
 
   public years: Array<any>;
   public masterDataSet: Array<IFlightResponse>;
@@ -30,7 +32,9 @@ export class LandingComponent implements OnInit {
 
   ngOnInit(): void {
     this.initialDataLoad = true;
-    this.getMasterData();
+    this.getMasterData().then(response => {
+      this.initialDataLoad = false;
+    });
   }
 
   // Method triggered while selecting the year.
@@ -46,94 +50,178 @@ export class LandingComponent implements OnInit {
       })
       year.activeColor = 'primary';
       if (this.validateSelectedYear(year.year)) {
-        this.filterDataBasedOnYear(year.year);
+        this.filterDataBasedOnYear(year.year).then(response => {
+          if (this.launchFail.isActive) {
+            this.masterDataSet = this.masterDataSet.filter(item => {
+              return !item.successfullLaunch;
+            })
+          } else if (this.landFail.isActive) {
+            this.masterDataSet = this.masterDataSet.filter(item => {
+              return !item.successfullLanding;
+            })
+          } else if (this.launchFail.isActive && this.landFail.isActive) {
+            this.masterDataSet = this.masterDataSet.filter(item => {
+              return !item.successfullLanding && !item.successfullLaunch;
+            })
+          }
+        });
       }
     }
   }
 
-  public toggleSuccessfullLaunch(event?) {
-    if (event.checked && this.land.checked) {
-      this.getsuccessfullLaunchAndLandData()
-    } else if (!event.checked && this.land.checked) {
-      this.getsuccessfullLaunchAndLandData()
-    } else if (event.checked) {
-      this.getsuccessfullLaunchData()
+  // Launch Specific Data
+  public launchSuccessful() {
+    this.launchTrue.isActive = !this.launchTrue.isActive;
+    this.launchFail.isActive = false;
+    this.filterApplied = true;
+    if (this.launchTrue.isActive && this.landTrue.isActive) {
+      this.getLaunchLandTrueData().then(response => {
+        this.masterDataSet = this.masterDataSet.filter(item => {
+          return item.successfullLaunch && item.successfullLanding;
+        })
+      })
+    } else if (this.launchTrue.isActive) {
+      this.getLaunchTrueData().then(response => {
+        if (this.landingService.selectedYear.value) {
+          this.filterDataBasedOnYear(this.landingService.selectedYear.value);
+        } else {
+          this.masterDataSet = [];
+          this.masterDataSet = this.missionDetailsCollection;
+        }
+        this.filterApplied = false;
+      })
     } else {
-      this.getMasterData()
+      this.getMasterData();
     }
   }
 
-  public toggleSuccessfullLanding(event) {
-    if (event.checked) {
-      this.getsuccessfullLaunchAndLandData()
-    } else if (event.checked && this.launch.checked) {
-      this.getsuccessfullLaunchAndLandData()
-    } else if (!event.checked && this.launch.checked) {
-      this.getsuccessfullLaunchData()
+  public launchFailed() {
+    this.launchTrue.isActive = false;
+    this.launchFail.isActive = !this.launchFail.isActive;
+    this.filterApplied = true;
+    this.getMasterData().then(response => {
+      if (this.launchFail.isActive) {
+        this.masterDataSet = this.masterDataSet.filter(item => {
+          return !item.successfullLaunch;
+        })
+      } else {
+        this.getMasterData();
+      }
+    });
+  }
+
+  private getLaunchTrueData(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.landingService.onSuccessLaunchTrue().subscribe(data => {
+        this.populateMasterData(data).then(response => {
+          return resolve(true);
+        });
+      })
+    })
+  }
+
+  public landSuccessful() {
+    this.landTrue.isActive = !this.landTrue.isActive;
+    this.landFail.isActive = false;
+    this.filterApplied = true;
+    if (this.landTrue.isActive) {
+      this.getLaunchLandTrueData().then(response => {
+        if (this.landingService.selectedYear.value) {
+          this.filterDataBasedOnYear(this.landingService.selectedYear.value);
+        } else {
+          this.masterDataSet = [];
+          this.masterDataSet = this.missionDetailsCollection;
+        }
+        this.filterApplied = false;
+      })
     } else {
-      this.getMasterData()
+      this.getMasterData();
     }
-    // event.checked ? this.getsuccessfullLaunchAndLandData() : this.getMasterData();
+  }
+
+  public landFailed() {
+    this.landTrue.isActive = false;
+    this.landFail.isActive = !this.landFail.isActive;
+    this.filterApplied = true;
+    this.getMasterData().then(response => {
+      if (this.landFail.isActive) {
+        this.masterDataSet = this.masterDataSet.filter(item => {
+          return !item.successfullLanding;
+        })
+      } else {
+        this.getMasterData();
+      }
+    });
+  }
+
+  private getLaunchLandTrueData(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.landingService.onSuccessLaunchAndLandTrue().subscribe(data => {
+        this.populateMasterData(data).then(response => {
+          return resolve(true);
+        });
+      })
+    })
   }
 
   // Basically main method to fetch non-filtered data.
-  private getMasterData() {
-    this.landingService.getMasterData().subscribe(response => {
-      this.populateMasterData(response, this.landingService.selectedYear.value);
-      this.initialDataLoad = false;
-      this.filterApplied = false;
-    });
+  private getMasterData(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.landingService.getMasterData().subscribe(response => {
+        this.populateMasterData(response, this.landingService.selectedYear.value).then(response => {
+          this.filterApplied = false;
+          return resolve(true);
+        });
+      });
+    })
+
   }
 
-  private getsuccessfullLaunchData() {
-    this.filterApplied = true;
-    this.landingService.onSuccessLaunchTrue().subscribe(result => {
-      this.populateMasterData(result, this.landingService.selectedYear.value);
-      this.filterApplied = false;
-    })
-  }
-
-  private getsuccessfullLaunchAndLandData() {
-    this.filterApplied = true;
-    this.landingService.onSuccessLaunchAndLandTrue().subscribe(result => {
-      this.populateMasterData(result, this.landingService.selectedYear.value);
-      this.filterApplied = false;
-    })
-  }
 
   // Basically an abstracted method to avoid repetetion of same logic in the above methods. Please refer to the call signatures of this method.
-  private populateMasterData(response: any, selectedYear?: string) {
-    let launchYears = [];
-    this.masterDataSet = [];
-    this.missionDetailsCollection = [];
-    this.years = [];
-    let set = new Set();
-    response.forEach(item => {
-      const responseObj = this.landingService.assignTemplateValues(item);
-      launchYears.push({ year: responseObj.launchYear });
-      this.missionDetailsCollection.push(responseObj);
-    });
-    if (this.landingService.originalYearsData.value.length === 0) {
-      launchYears.forEach(item => {
-        if (!set.has(item.year)) {
-          set.add(item.year);
-          this.years.push(item);
-        }
+  private populateMasterData(response: any, selectedYear?: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      let launchYears = [];
+      this.masterDataSet = [];
+      this.missionDetailsCollection = [];
+      this.years = [];
+      let set = new Set();
+      response.forEach(item => {
+        const responseObj = this.landingService.assignTemplateValues(item);
+        launchYears.push({ year: responseObj.launchYear });
+        this.missionDetailsCollection.push(responseObj);
       });
-      this.landingService.originalYearsData.next(this.years);
-    } else {
-      this.years = this.landingService.originalYearsData.value;
-    }
-    this.masterDataSet = this.missionDetailsCollection;
-    this.filterDataBasedOnYear(selectedYear);
+      if (this.landingService.originalYearsData.value.length === 0) {
+        launchYears.forEach(item => {
+          if (!set.has(item.year)) {
+            set.add(item.year);
+            this.years.push(item);
+          }
+        });
+        this.landingService.originalYearsData.next(this.years);
+      } else {
+        this.years = this.landingService.originalYearsData.value;
+      }
+      this.masterDataSet = this.missionDetailsCollection;
+      if (selectedYear) {
+        this.filterDataBasedOnYear(selectedYear).then(response => {
+          return resolve(true);
+        });
+      } else {
+        return resolve(true);
+      }
+    })
   }
 
-  private filterDataBasedOnYear(year?) {
-    if (year) {
-      this.masterDataSet = this.missionDetailsCollection.filter(item => {
-        return item.launchYear === year;
-      })
-    }
+  private filterDataBasedOnYear(year?): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (year) {
+        this.masterDataSet = this.missionDetailsCollection.filter(item => {
+          return item.launchYear === year;
+        })
+      }
+      return resolve(true);
+    });
   }
 
   private validateSelectedYear(selectedYear): boolean {
